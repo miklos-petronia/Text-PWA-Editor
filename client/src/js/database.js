045793fc41889fc1bcf739360429961b36affab1
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-06 18:10:11 +0100

    Argumentation to a approach that obtain stories and insert it to the database application

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -11,3 +11,13 @@ const initdb = async () =>
             console.log('jate database created');
         },
     });
+
+// Argumentation to a approach that obtain stories and insert it to the database application
+export const putDb = async (content) => {
+    const jateDB = await openDB("jate", 1);
+    const tx = jateDB.transaction("jate", "readwrite");
+    const store = tx.objectStore("jate");
+    const request = store.put({ id: 1, value: content });
+    const result = await request;
+    console.log("Data saved", result);
+};
