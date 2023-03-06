65d39bb720e4511ff830dd88ebb9f329b6c80251
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-06 18:16:26 +0100

    Analyse if CodeMirror is working

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -5,3 +5,19 @@ import { header } from './header';
 export default class {
     constructor() {
         const localData = localStorage.getItem('content');
+
+// Analyse if CodeMirror is working
+        if (typeof CodeMirror === 'undefined') {
+            throw new Error('CodeMirror is not loaded');
+        }
+
+        this.editor = CodeMirror(document.querySelector('#main'), {
+            value: '',
+            mode: 'javascript',
+            theme: 'monokai',
+            lineNumbers: true,
+            lineWrapping: true,
+            autofocus: true,
+            indentUnit: 2,
+            tabSize: 2,
+        });
