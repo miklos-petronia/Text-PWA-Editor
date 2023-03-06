edb6e05e55fa09ca1c72fb5a8d1dd81e4ed30f33
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-06 19:02:00 +0100

    Perform a click operator on the  domain and console.log prompt event

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -5,3 +5,11 @@ window.addEventListener('beforeinstallprompt', (event) => {
     window.deferredPrompt = event;
     butInstall.classList.toggle("hidden", false);
 });
+
+// Perform a click operator on the `butInstall` domain
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    // console.log(promptEvent)
+    if (!promptEvent) {
+        return;
+    }
