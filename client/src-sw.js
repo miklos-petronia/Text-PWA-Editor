00b063ae0db23960afb9348abd0319b662b2dc55
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-06 20:54:11 +0100

    precache and route self wb manifest

---
+++ client/src-sw.js
@@ -0,0 +1,21 @@
+const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst } = require('workbox-strategies');
+const { registerRoute } = require('workbox-routing');
+const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+const { ExpirationPlugin } = require('workbox-expiration');
+const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { StaleWhileRevalidate } = require('workbox-strategies');
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+const pageCache = new CacheFirst({
+    cacheName: 'page-cache',
+    plugins: [
+        new CacheableResponsePlugin({
+            statuses: [0, 200],
+        }),
+        new ExpirationPlugin({
+            maxAgeSeconds: 30 * 24 * 60 * 60,
+        }),
+    ],
+});
