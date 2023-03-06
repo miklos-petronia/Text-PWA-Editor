27a29e4a7ed8f8920bb32ed33bbe6889aa643f0e
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-06 18:20:25 +0100

    When the editor is accessible, set the value which is gathered in indexeddb and Fall back to localStorage  and set the value to header if nothing is stored in indexeddb or is available.

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -21,3 +21,13 @@ export default class {
             indentUnit: 2,
             tabSize: 2,
         });
+
+        // When the editor is accessible, set the value which is gathered in indexeddb and Fall back to localStorage  and set the value to header if nothing is stored in indexeddb or is available.
+        getDb().then((data) => {
+            console.info('Loaded data from IndexedDB, injecting into editor');
+            this.editor.setValue(data || localData || header);
+        });
+
+        this.editor.on('change', () => {
+            localStorage.setItem('content', this.editor.getValue());
+        });
